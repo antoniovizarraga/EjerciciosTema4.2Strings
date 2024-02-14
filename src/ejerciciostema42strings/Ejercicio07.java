dc36456fd4a5bf37c1778fce636b32dcbe11c6a7
antoniovizarraga <145675344+antoniovizarraga@users.noreply.github.com>
2024-02-14 10:10:41 +0100

    Ejercicio 7 terminado

---
+++ src/ejerciciostema42strings/Ejercicio07.java
@@ -0,0 +1,21 @@
+package ejerciciostema42strings;
+
+/**
+ * Crearemos un programa con una función que reciba por parámetros una frase y una palabra.
+ * Debe devolver cuántas veces aparece la palabra en la frase.
+ */
+public class Ejercicio07 {
+
+	public static String wordsCounter(String phrase, String word) {
+		
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
